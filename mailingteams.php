e637a4e7f4b0f5282882ac29a2bd06d28495b54c
Francis Whittle <francis@agileware.com.au>
2017-09-01 12:29:20 +1000

    GREENS-11: Make additions to Team settings form functional.

--- mailingteams.php
+++ mailingteams.php
@@ -139,17 +139,23 @@ function mailingteams_civicrm_entityTypes(&$entityTypes) {
   }
 }
 
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
 function mailingteams_civicrm_preProcess($formName, &$form) {
   if ($formName == 'CRM_Team_Form_Settings') {
+    /* Add the MailingTeams Group and Elements to the form. */
     $form->add_elementGroup('mailingteams', ts('Mailing'));
 
-    $form->add('checkbox', 'mailingteams.restricted', ts('Restricted'));
+    $form->add('checkbox', 'mailingteams_restricted', ts('Restricted'));
     $form->add_elementName(
-      'mailingteams.restricted', 'mailingteams',
+      'mailingteams_restricted', 'mailingteams',
       ts('A restricted Team cannot load or copy Mailings created by another Team.')
     );
 
-    $form->addEntityRef('mailingteams.from_addresses', ts('From Email Addresses'),
+    $form->addEntityRef('mailingteams_from_addresses', ts('From Email Addresses'),
       array (
         'entity' => 'option_value',
         'api' => array ('params' => array ('option_group_id' => 'from_email_address')),
@@ -157,7 +163,7 @@ function mailingteams_civicrm_preProcess($formName, &$form) {
         'multiple' => TRUE,
       ));
     $form->add_elementName(
-      'mailingteams.from_addresses', 'mailingteams',
+      'mailingteams_from_addresses', 'mailingteams',
       ts('From Email Addresses that this Team is allowed to create Mailings with. The list of available email addresses is administered from <a href="%1">%2</a>',
         array(
           1 => CRM_Utils_System::url('civicrm/admin/options/from_email_address', 'reset=1'),
@@ -165,27 +171,54 @@ function mailingteams_civicrm_preProcess($formName, &$form) {
         )));
 
     // Generate the magic incantation to find only mailing type groups.
-    // @TODO stab my eyeballs out with a spoon.
     $mailing_type = civicrm_api3('OptionValue', 'getvalue',
       array('option_group_id' => 'group_type', 'name' => 'Mailing List', 'return' => 'value')
     );
     $mailing_type = "%\x01{$mailing_type}\x01%";
 
-    $form->addEntityRef('mailingteams.groups_draft', ts('Draft Groups'),
+    $form->addEntityRef('mailingteams_groups_draft', ts('Draft Groups'),
       array (
         'entity' => 'group',
         'api' => array('params' => array( 'group_type' => array('LIKE' => $mailing_type))),
         'multiple' => TRUE,
       ));
-    $form->add_elementName('mailingteams.groups_draft', 'mailingteams', ts('Groups that this Team can draft Mailings for.'));
+    $form->add_elementName('mailingteams_groups_draft', 'mailingteams', ts('Groups that this Team can Draft Mailings for.'));
 
-    $form->addEntityRef('mailingteams.groups_publish', ts('Publish Groups'),
+    $form->addEntityRef('mailingteams_groups_publish', ts('Publish Groups'),
       array (
         'entity' => 'group',
         'api' => array('params' => array( 'group_type' => array('LIKE' => $mailing_type))),
         'multiple' => TRUE,
       ));
-    $form->add_elementName('mailingteams.groups_publish', 'mailingteams', ts('Groups that this Team can approve and Schedule for Mailings.'));
+    $form->add_elementName('mailingteams_groups_publish', 'mailingteams', ts('Groups that this Team can Approve and Schedule Mailings for.'));
+
+    // Get and set the MailingTeam defaults.
+    $team = civicrm_api3('Team','getsingle', array('id' => $form->team_id));
+    $tfea = civicrm_api3('TeamMailingFromAddress', 'get', array('team_id' => $form->team_id, 'options' => array('limit' => 0)));
+    $tgr  = civicrm_api3('TeamMailingGroup', 'get', array('team_id' => $form->team_id, 'options' => array('limit' => 0)));
+
+    $defaults['mailingteams_from_addresses'] = array();
+    $defaults['mailingteams_groups_draft']   = array();
+    $defaults['mailingteams_groups_publish'] = array();
+
+    foreach($tfea['values'] as $e) {
+      $defaults['mailingteams_from_addresses'][] = $e['from_email_address_id'];
+    }
+    foreach($tgr['values'] as $g) {
+      if($g['role'] == 'draft') {
+        $defaults['mailingteams_groups_draft'][] = $g['group_id'];
+      }
+      elseif($g['role'] == 'publish') {
+        $defaults['mailingteams_groups_publish'][] = $g['group_id'];
+      }
+    }
+
+    $defaults['mailingteams_restricted']     = !empty($team['data']['mailingteams']['restricted']);
+    $defaults['mailingteams_from_addresses'] = implode(',', $defaults['mailingteams_from_addresses']);
+    $defaults['mailingteams_groups_draft']   = implode(',', $defaults['mailingteams_groups_draft']);
+    $defaults['mailingteams_groups_publish'] = implode(',', $defaults['mailingteams_groups_publish']);
+
+    $form->setDefaults($defaults);
   }
 }
 
@@ -193,7 +226,137 @@ function mailingteams_civicrm_postProcess($formName, &$form) {
   if ($formName == 'CRM_Team_Form_Settings') {
     $values = $form->exportValues();
 
-    $team = civicrm_api3('Team', 'getsingle', array('id' => $values['team_id']));
+    $team = civicrm_api3('Team', 'getsingle', array('id' => $form->team_id));
+
+    $data = &$team['data'];
+
+    // Flag if we need to update the data field of the Team
+    $update_data = FALSE;
+
+    if((!empty($values['mailingteams_restricted']) &&  empty($data['mailingteams']['restricted'])) ||
+         empty($values['mailingteams_restricted']) && !empty($data['mailingteams']['restricted'])) {
+      $data['mailingteams']['restricted'] = !empty($values['mailingteams_restricted']);
+      $update_data = TRUE;
+    }
+
+    if($update_data) {
+      // Yes we have data updates! Apply them.
+      civicrm_api3('Team', 'create', array('id' => $form->team_id, 'data' => $data));
+    }
+
+    $from_addresses = (!empty($values['mailingteams_from_addresses'])
+                      ? explode(',', $values['mailingteams_from_addresses'])
+                      : array());
+    _mailingteams_update_from_addresses($form->team_id, $from_addresses);
+
+    $draft_groups   = (!empty($values['mailingteams_groups_draft'])
+                      ? explode(',', $values['mailingteams_groups_draft'])
+                      : array());
+
+    $publish_groups = (!empty($values['mailingteams_groups_publish'])
+                      ? explode(',', $values['mailingteams_groups_publish'])
+                      : array());
+
+    _mailingteams_update_groups($form->team_id, $draft_groups, $publish_groups);
+  }
+}
+
+/**
+ * "Internal" function for updating from addresses from a list of from address ids.
+ *
+ * @param int $team_id
+ * @param array $from_addresses
+ *
+ **/
+function _mailingteams_update_from_addresses($team_id, $from_addresses) {
+  try {
+    $team_from_addresses = civicrm_api3('TeamMailingFromAddress', 'get', array('team_id' => $team_id));
+    $team_from_addresses = $team_from_addresses['values'];
+  }
+  catch(CiviCRM_API3_Exception $e){
+    $team_from_addresses = array();
+  }
+
+  $tfa_match = array();
+
+  // Start with known address ids.
+  foreach($team_from_addresses as $id => $tfa) {
+    if (!in_array($tfa['from_email_address_id'], $from_addresses)) {
+      // Delete those that do not match input.
+      civicrm_api3('TeamMailingFromAddress', 'delete', array('id' => $id));
+    }
+    else {
+      // Mark those that match input as not requiring update.
+      $tfa_match[$tfa['from_email_address_id']] = TRUE;
+    }
+  }
+
+  // Loop through input to find new addresses ids
+  foreach($from_addresses as $eid) {
+    if(empty($tfa_match[$eid])) {
+      civicrm_api3('TeamMailingFromAddress', 'create', array('team_id' => $team_id, 'from_email_address_id' => $eid));
+    }
+  }
+}
+
+/**
+ * "Internal" function for updating groups links from lists of from group ids.
+ *
+ * @param int $team_id
+ * @param array $draft_groups
+ * @param array $publish_groups
+ *
+ **/
+function _mailingteams_update_groups($team_id, $draft_groups, $publish_groups) {
+  try {
+    $team_groups = civicrm_api3('TeamMailingGroup', 'get', array('team_id' => $team_id));
+    $team_groups = $team_groups['values'];
+  }
+  catch(CiviCRM_API3_Exception $e){
+    $team_groups = array();
+  }
+
+  $dgr_match = array();
+  $pgr_match = array();
+
+  // Start with known groups.
+  foreach($team_groups as $id => $tgr) {
+    switch($tgr['role']){
+      case 'draft':
+        if (!in_array($tgr['group_id'], $draft_groups)) {
+          // Delete those that do not match input.
+          civicrm_api3('TeamMailingGroup', 'delete', array('id' => $id));
+        }
+        else {
+          // Mark as not requiring update.
+          $dgr_match[$tgr['group_id']] = TRUE;
+        }
+        break;
+      case 'publish':
+        if (!in_array($tgr['group_id'], $publish_groups)) {
+                  // Delete those that do not match input.
+          civicrm_api3('TeamMailingGroup', 'delete', array('id' => $id));
+        }
+        else {
+          // Mark as not requiring update.
+          $pgr_match[$tgr['group_id']] = TRUE;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  // Loop through input to find new groups' ids
+  foreach($draft_groups as $gid) {
+    if(empty($dgr_match[$gid])) {
+      civicrm_api3('TeamMailingGroup', 'create', array('team_id' => $team_id, 'group_id' => $gid, 'role' => 'draft'));
+    }
+  }
+  foreach($publish_groups as $gid) {
+    if(empty($pgr_match[$gid])) {
+      civicrm_api3('TeamMailingGroup', 'create', array('team_id' => $team_id, 'group_id' => $gid, 'role' => 'publish'));
+    }
   }
 }
 
