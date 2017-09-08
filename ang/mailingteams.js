b1bde0ff87972be4c9f5a47560dc688c21fa35df
Francis Whittle <francis@agileware.com.au>
2017-09-08 17:41:32 +1000

    GREENS-4: Make angular directive set a canary flag in the Recipients AJAX Transport.

--- ang/mailingteams.js
+++ ang/mailingteams.js
@@ -1,6 +1,6 @@
 (function(angular, $, _) {
   // Declare a list of dependencies.
   angular.module('mailingteams', [
-    'crmUi', 'crmUtil', 'ngRoute'
+    'crmUi', 'crmUtil', 'ngRoute', 'crmMailing'
   ]);
 })(angular, CRM.$, CRM._);
--- ang/mailingteams/crmMailingRecipients.js
+++ ang/mailingteams/crmMailingRecipients.js
@@ -1,26 +1,43 @@
 (function(angular, $, _) {
-  // "crmMailingRecipients" is a basic skeletal directive.
-  // Example usage: <div crm-mailing-recipients="{foo: 1, bar: 2}"></div>
   angular.module('mailingteams').directive('crmMailingRecipients', function() {
-    console.log('I am a 🐟.');
-  
+
+    /**
+     * Utility function wrapping the jQuery transport to add our marker in.
+     */
+    function select2_transport_wrap(select_2) {
+      let oTransport = select_2.opts.ajax.transport;
+
+      select_2.opts.ajax.transport = function(params) {
+        // Just add an extra parameter.  We'll pick this up in the API Wrapper.
+        // See class CRM_MailingTeam_APIWrapper.
+        params.data.params.forMailing = true;
+        return oTransport(params);
+      }
+    }
+
     return {
-      //restrict: 'AE',
-      //templateUrl: '~/mailingteams/crmMailingRecipients.html',
-      //scope: { crmMailingRecipients: '=' },
-      link: function($scope, $el, $attr) {
-        var ts = $scope.ts = CRM.ts('mailingteams');
-        $scope.$watch('crmMailingRecipients', function(newValue){
-          $scope.myOptions = newValue;
-        });
+      // Add an additional link function to the crmMailingRecipients directive
+      // that invoikes our wrapper code.
+      link: function(scope, element, attrs, ngModel) {
+        if(select_2 = element.data('select2')) {
+          // If the select2 has already been initialized, go ahead and wrap it now.
+          select2_transport_wrap(select_2);
+        } else {
+          // Otherwise proxy the data function
+          let oData = CRM.$.fn.data;
+
+          CRM.$.fn.data = function () {
+            if(this[0] == element[0] && arguments.length > 1) {
+              select_2 = arguments[1];
+              select2_transport_wrap(arguments[1]);
+
+              // Once we've wrapped, we no longer need the proxy, so remove it.
+              CRM.$.fn.data = oData;
+            }
+            return oData.apply(this, arguments);
+          }
+        }
       }
     };
   });
-
- /* angular.module('mailingteams').decorator(
-    'crmMailingRecipients',
-    $delegate => {
-      console.log($delegate);
-    }
-  )*/
 })(angular, CRM.$, CRM._);
